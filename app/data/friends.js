6f36fcebc761d6b73578c83d81da0605c7e1fc3c
Brett O'Connell <apocreativeworks@gmail.com>
2018-12-05 19:10:34 -0600

    ahhhh

---
+++ app/data/friends.js
@@ -0,0 +1,91 @@
+var friendArray = [
+
+{
+    "name":"Ahmed",
+    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores":[
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+      ]
+  },
+  {
+
+  "name":"Betty",
+    "photo":"https://cdn.washingtoncitypaper.com/files/base/scomm/wcp/image/2009/04/640w/__contexts.org_socimages_files_2009_04_d_silhouette.jpg",
+    "scores":[
+        3,
+        1,
+        5,
+        2,
+        1,
+        3,
+        4,
+        4,
+        4,
+        4
+      ]
+  },
+
+  {
+
+    "name":"Carlos",
+      "photo":"https://cdn.washingtoncitypaper.com/files/base/scomm/wcp/image/2009/04/640w/__contexts.org_socimages_files_2009_04_d_silhouette.jpg",
+      "scores":[
+          1,
+          2,
+          3,
+          4,
+          5,
+          4,
+          3,
+          2,
+          1,
+          2
+        ]
+    },
+
+    {
+
+        "name":"Darla",
+          "photo":"https://cdn.washingtoncitypaper.com/files/base/scomm/wcp/image/2009/04/640w/__contexts.org_socimages_files_2009_04_d_silhouette.jpg",
+          "scores":[
+              5,
+              5,
+              4,
+              3,
+              2,
+              2,
+              1,
+              2,
+              1,
+              3
+            ]
+        },
+
+        {
+
+            "name":"Edith",
+              "photo":"https://cdn.washingtoncitypaper.com/files/base/scomm/wcp/image/2009/04/640w/__contexts.org_socimages_files_2009_04_d_silhouette.jpg",
+              "scores":[
+                  1,
+                  1,
+                  3,
+                  5,
+                  5,
+                  5,
+                  3,
+                  1,
+                  2,
+                  4
+                ]
+            }
+
+];
